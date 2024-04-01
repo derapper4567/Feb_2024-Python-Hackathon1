5a1cbbaa87be090820b182553e7b9842c2c4bce8
unknown <ashakisonga2022@gmail.com>
2024-04-01 22:43:28 +0300

    user input

---
+++ USER_input.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+location = input("Enter your location: ")
+
+# Print personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
